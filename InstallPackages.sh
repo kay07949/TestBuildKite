742050f2e1d84953a75d6e3da6addfce96732ce3
Kay <kay07949@yahoo.co.uk>
2021-01-16 02:43:52 +0000

    add

---
+++ InstallPackages.sh
@@ -0,0 +1,15 @@
+apt-get update && sudo apt-get install -y \
+     build-essential \
+     curl \
+     git \
+     libfreetype6-dev \
+     libpng12-dev \
+     libzmq3-dev \
+     pkg-config \
+     python-dev \
+     python-numpy \
+     python-pip \
+     software-properties-common \
+     swig \
+     zip \
+     zlib1g-dev
--- InstallPowershell.sh
+++ InstallPowershell.sh
@@ -1,1 +1,14 @@
-sudo apt install curl
+# Update the list of packages
+apt-get update
+# Install pre-requisite packages.
+apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+# Register the Microsoft repository GPG keys
+dpkg -i packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
+apt-get update
+# Install PowerShell
+apt-get install -y powershell
+# Start PowerShell
+pwsh
